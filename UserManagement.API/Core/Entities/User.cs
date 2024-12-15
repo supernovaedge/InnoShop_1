cb313945bd17fc03796cc12891ed15637b4e7908
Nika Gurchiani <104095410+supernovaedge@users.noreply.github.com>
2024-12-15 23:39:36 +0100

    defined domain entity and interface

---
+++ UserManagement.API/Core/Entities/User.cs
@@ -0,0 +1,14 @@
+namespace UserManagement.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
